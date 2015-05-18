4069df22028fee452ff5b70bed4f1c3cf52bfb3f
Eliot Hedeman <ehedeman@barracuda.com>
2015-05-17 22:36:56 -0400

    added ingestion for http and tcp

--- cmd/bangarang/main.go
+++ cmd/bangarang/main.go
@@ -1,7 +1,14 @@
 package main
 
-import _ "github.com/eliothedeman/bangarang/alarm/console"
+import (
+	_ "github.com/eliothedeman/bangarang/alarm/console"
+	"github.com/eliothedeman/bangarang/pipeline"
+)
 
 func main() {
+	tcp := 8080
+	p := pipeline.NewPipeline(&tcp, nil)
+	p.Start()
+	<-make(chan struct{})
 
 }
--- pipeline/pipeline.go
+++ pipeline/pipeline.go
@@ -1,16 +1,103 @@
 package pipeline
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"net"
+	"net/http"
 
 	"github.com/eliothedeman/bangarang/alarm"
 	"github.com/eliothedeman/bangarang/event"
 )
 
 type Pipeline struct {
-	port        int
-	escalations []*alarm.Escalation
-	index       *event.Index
+	tcpPort, httpPort *int
+	escalations       []*alarm.Escalation
+	index             *event.Index
+}
+
+func NewPipeline(tcpPort, httpPort *int) *Pipeline {
+	return &Pipeline{
+		tcpPort:  tcpPort,
+		httpPort: httpPort,
+		index:    event.NewIndex(),
+	}
+}
+
+func (p *Pipeline) Start() {
+	go p.IngestHttp()
+	go p.IngestTcp()
+}
+
+func (p *Pipeline) IngestHttp() {
+	if p.httpPort == nil {
+		return
+	}
+	http.HandleFunc("/ingest", func(w http.ResponseWriter, r *http.Request) {
+		buff, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		e := &event.Event{}
+
+		err = json.Unmarshal(buff, e)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		p.Process(e)
+	})
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *p.httpPort), nil))
+}
+
+func (p *Pipeline) consumeTcp(c *net.TCPConn) {
+	buff := make([]byte, 1024*200)
+	for {
+		n, err := c.Read(buff)
+		log.Println(string(buff))
+		if err != nil {
+			log.Println(err)
+			c.Close()
+			return
+		}
+		e := &event.Event{}
+		err = json.Unmarshal(buff[:n], e)
+		if err != nil {
+			log.Println(err)
+		} else {
+			p.Process(e)
+		}
+
+	}
+}
+
+func (p *Pipeline) IngestTcp() {
+	if p.tcpPort == nil {
+		return
+	}
+	addr, err := net.ResolveTCPAddr("tcp4", fmt.Sprintf(":%d", *p.tcpPort))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c, err := net.ListenTCP("tcp", addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := c.AcceptTCP()
+		if err != nil {
+			log.Println(err)
+		} else {
+			go p.consumeTcp(conn)
+		}
+	}
 }
 
 func (p *Pipeline) Process(e *event.Event) int {
