16f5dd75ba79234d4f415e4111498a3bf2b83dee
eliothedeman <eliot.d.hedeman@gmail.com>
2015-05-22 14:00:54 -0400

    initial add of statistical tracking of events

--- alarm/condition.go
+++ alarm/condition.go
@@ -1,26 +1,48 @@
 package alarm
 
 import (
+	"math"
 	"sync"
 	"sync/atomic"
 
 	"github.com/eliothedeman/bangarang/event"
+	"github.com/eliothedeman/smoothie"
+)
+
+//go:generate ffjson $GOFILE
+
+var (
+	DEFAULT_WINDOW_SIZE = 100
 )
 
 type Condition struct {
 	Greater      *float64                 `json:"greater"`
 	Less         *float64                 `json:"less"`
 	Exactly      *float64                 `json:"exactly"`
+	StdDev       *StdDev                  `json:"std_dev"`
 	Occurences   int                      `json:"occurences"`
 	tracker      map[string]*EventTracker `-`
 	trackerMutex sync.RWMutex
+}
+
+type StdDev struct {
+	Sigma      float64 `json:"sigma"`
+	WindowSize *int    `json:"window_size"`
 }
 
 // start tracking an event, and returns if the event has hit it's occurence settings
 func (c *Condition) TrackEvent(e *event.Event) bool {
 	c.initTracker()
 	t := c.getEventTracker(e)
-	t.Inc()
+
+	if c.trackingStats() {
+		t.df.Push(e.Metric)
+	}
+
+	if c.Satisfies(e) {
+		t.Inc()
+	}
+
 	return t.Occurences() >= c.Occurences
 }
 
@@ -32,12 +54,22 @@ func (c *Condition) CleanEvent(e *event.Event) {
 
 }
 
+func (c *Condition) trackingStats() bool {
+	return c.StdDev != nil
+}
+
 func (c *Condition) getEventTracker(e *event.Event) *EventTracker {
 	c.trackerMutex.RLock()
 	t, ok := c.tracker[e.IndexName()]
 	c.trackerMutex.RUnlock()
 	if !ok {
-		t = NewEventTracker(e)
+		t = NewEventTracker()
+		if c.trackingStats() {
+			if c.StdDev.WindowSize == nil {
+				c.StdDev.WindowSize = &DEFAULT_WINDOW_SIZE
+			}
+			t.initDataFrame(*c.StdDev.WindowSize)
+		}
 		c.trackerMutex.Lock()
 		c.tracker[e.IndexName()] = t
 		c.trackerMutex.Unlock()
@@ -47,22 +79,24 @@ func (c *Condition) getEventTracker(e *event.Event) *EventTracker {
 }
 
 func (c *Condition) initTracker() {
-	c.trackerMutex.RLock()
 	if c.tracker == nil {
+		c.trackerMutex.Lock()
 		c.tracker = make(map[string]*EventTracker)
+		c.trackerMutex.Unlock()
 	}
-	c.trackerMutex.RUnlock()
 }
 
 type EventTracker struct {
-	event     *event.Event
+	df        *smoothie.DataFrame
 	occurence int64
 }
 
-func NewEventTracker(e *event.Event) *EventTracker {
-	return &EventTracker{
-		event: e,
-	}
+func (e *EventTracker) initDataFrame(windowSize int) {
+	e.df = smoothie.EmptyDataFrame(windowSize)
+}
+
+func NewEventTracker() *EventTracker {
+	return &EventTracker{}
 }
 
 func (e *EventTracker) Inc() {
@@ -94,6 +128,20 @@ func (c *Condition) Satisfies(e *event.Event) bool {
 	if c.Exactly != nil {
 		if e.Metric == *c.Exactly {
 			return true
+		}
+	}
+
+	if c.trackingStats() {
+
+		// check if the current metric is outside of n * sigma
+		if c.StdDev != nil {
+			c.trackerMutex.RLock()
+			t := c.tracker[e.IndexName()]
+			avg := t.df.Avg()
+			if math.Abs(e.Metric-avg) > t.df.StdDev()*c.StdDev.Sigma {
+				return true
+			}
+			c.trackerMutex.RUnlock()
 		}
 	}
 
