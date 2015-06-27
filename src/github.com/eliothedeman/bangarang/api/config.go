9d0ffb3835fbfbf18dcc1454399c67e7c52a6afe
Eliot Hedeman <eliot.d.hedeman@gmail.com>
2015-06-27 02:24:59 -0400

    docs

--- src/github.com/eliothedeman/bangarang/api/config.go
+++ src/github.com/eliothedeman/bangarang/api/config.go
@@ -9,25 +9,29 @@ import (
 	"github.com/eliothedeman/bangarang/pipeline"
 )
 
-// handles the api methods for incidents
+// ConfigHash handles the api methods for incidents
 type ConfigHash struct {
 	pipeline *pipeline.Pipeline
 }
 
+// NewConfigHash Create a new ConfigHash api method
 func NewConfigHash(pipe *pipeline.Pipeline) *ConfigHash {
 	return &ConfigHash{
 		pipeline: pipe,
 	}
 }
 
-func (i *ConfigHash) EndPoint() string {
+// EndPoint return the endpoint of this method
+func (c *ConfigHash) EndPoint() string {
 	return "/api/config/hash"
 }
 
+// HashResponse will be marshaled as json as the successful response
 type HashResponse struct {
 	Hash string `json:"hash"`
 }
 
+// Get HTTP get method
 func (c *ConfigHash) Get(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Add("content-type", "application/json")
@@ -42,5 +46,7 @@ func (c *ConfigHash) Get(w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 		return
 	}
+	w.Header()
+
 	w.Write(buf)
 }
