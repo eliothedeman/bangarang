e79a0cd966e4f670efe42d17bf76c98a71b6063a
Eliot Hedeman <eliot.d.hedeman@gmail.com>
2015-08-21 01:09:44 -0400

    added new tests for standard deviations

---
+++ src/github.com/eliothedeman/bangarang/alarm/std_dev_test.go
@@ -0,0 +1,78 @@
+package alarm
+
+import (
+	"math/rand"
+	"testing"
+)
+
+func TestStdDevFalsePositive(t *testing.T) {
+	c := &Condition{
+		Greater:    test_f(5),
+		StdDev:     true,
+		WindowSize: 100,
+	}
+
+	c.init(DEFAULT_GROUP_BY)
+	for i := 0; i < 1000; i++ {
+		e := newTestEvent("machine.test.com", "test_service", rand.Float64()*10)
+		if c.TrackEvent(e) {
+			c.DoOnTracker(e, func(et *eventTracker) {
+				t.Fatal(et.df.Data())
+			})
+		}
+	}
+}
+
+func TestStdDevQuarter(t *testing.T) {
+	c := &Condition{
+		Greater:    test_f(1),
+		StdDev:     true,
+		WindowSize: 100,
+	}
+
+	c.init(DEFAULT_GROUP_BY)
+
+	// insert less than 1/4 of the window size
+	for i := 0; i < 20; i++ {
+		e := newTestEvent("machine.test.com", "test_service", rand.Float64()*10)
+		if c.TrackEvent(e) {
+			c.DoOnTracker(e, func(et *eventTracker) {
+				t.Fatal(et.df.Data())
+			})
+		}
+	}
+
+	// now insert something that is wayyyy out of the std_dev
+	e := newTestEvent("machine.test.com", "test_service", 10000000.0)
+	if c.TrackEvent(e) {
+		c.DoOnTracker(e, func(et *eventTracker) {
+			t.Fatal(et.df.Data())
+		})
+	}
+}
+
+func TestStdDev(t *testing.T) {
+	c := &Condition{
+		Greater:    test_f(1),
+		StdDev:     true,
+		WindowSize: 100,
+	}
+
+	c.init(DEFAULT_GROUP_BY)
+
+	for i := 0; i < 25; i++ {
+		e := newTestEvent("machine.test.com", "test_service", rand.Float64()*10)
+		if c.TrackEvent(e) {
+			c.DoOnTracker(e, func(et *eventTracker) {
+				t.Fatal(et.df.Data())
+			})
+		}
+	}
+	e := newTestEvent("machine.test.com", "test_service", 1000000000.0)
+	if !c.TrackEvent(e) {
+		c.DoOnTracker(e, func(et *eventTracker) {
+			t.Fatal(et.df.Data())
+		})
+	}
+
+}
