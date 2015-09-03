cc845a4396e6862ce8e59bb9cccf276d5c89362f
Eliot Hedeman <eliot.d.hedeman@gmail.com>
2015-09-03 18:44:41 -0400

    updated docs

--- src/github.com/eliothedeman/bangarang/event/event.go
+++ src/github.com/eliothedeman/bangarang/event/event.go
@@ -15,6 +15,8 @@ const (
 //go:generate ffjson $GOFILE
 //go:generate msgp $GOFILE
 
+// Event represents a metric as it passes through the pipeline.
+// it holds meta data about the metric, as well as methods to trace the event as it is processed
 type Event struct {
 	Host       string            `json:"host" msg:"host"`
 	Service    string            `json:"service" msg:"service"`
